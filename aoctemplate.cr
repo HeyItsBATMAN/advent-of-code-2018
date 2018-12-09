ce1d238e2e8d17fb5e9f16120e13c87fcf87c6d1
Kai Niebes <kai.niebes@outlook.com>
2018-12-09 18:44:39 +0100

    Add crystal template

---
+++ aoctemplate.cr
@@ -0,0 +1,41 @@
+infile = "../aoc#{PROGRAM_NAME.split('.')[PROGRAM_NAME.split('.').size - 2].split("aoc")[1]}.txt"
+input = File.new(infile).gets_to_end
+
+Solution.new input
+
+class Solution
+  def initialize(@input : String)
+    self.run
+  end
+
+  def run
+    time1 = Time.monotonic
+    part1res = self.part1
+    time2 = Time.monotonic
+    puts "Part 1:\t#{part1res} in #{self.printTime(time2 - time1)}"
+    time1 = Time.monotonic
+    part2res = self.part2
+    time2 = Time.monotonic
+    puts "Part 2:\t#{part2res} in #{self.printTime(time2 - time1)}"
+  end
+
+  def printTime(time : Time::Span)
+    if time.seconds > 1
+      return "#{time.seconds}s"
+    elsif time.milliseconds > 1
+      return "#{time.milliseconds}ms"
+    elsif time.microseconds > 1
+      return "#{time.microseconds}µs"
+    else
+      return "#{time.nanoseconds}ns"
+    end
+  end
+
+  def part1
+    return @input
+  end
+
+  def part2
+    return @input
+  end
+end
