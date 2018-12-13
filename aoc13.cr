a3dd183213870cb76f4d1967b409336397be3b6c
Kai Niebes <kai.niebes@outlook.com>
2018-12-13 11:51:01 +0100

    AoC13.cr: Refactored cart movement into the Cart Class

--- aoc13.cr
+++ aoc13.cr
@@ -4,7 +4,12 @@ input = File.new(infile).gets_to_end.split("\n")
 Solution.new input
 
 class Solution
+  @carts : Array(Cart)
+  @coords : Array(Array(Char))
+
   def initialize(@input : Array(String))
+    @carts = Array(Cart).new
+    @coords = @input.dup.map { |line| line.chars }
     self.run
   end
 
@@ -12,7 +17,10 @@ class Solution
     time1 = Time.monotonic
     solution = self.solution
     time2 = Time.monotonic
-    puts "Result:\t#{solution} in #{self.print_time(time2 - time1)}"
+    puts "Result:"
+    puts "First intersection:\t#{solution["First intersection"]}"
+    puts "Last remaining:\t\t#{solution["Last remaining"]}"
+    puts "in #{self.print_time(time2 - time1)}"
   end
 
   def print_time(time : Time::Span)
@@ -35,136 +43,133 @@ class Solution
   end
 
   class Cart
-    property step, x, y, below, dir, moved, crashed
+    property step, x, y, dir, moved, crashed, below
 
     def initialize(x : Int32, y : Int32, below : Char, current : Direction)
       @step = 0
+      @below = below
       @moved = false
       @crashed = false
       @y = y
       @x = x
-      @below = below
       @dir = current
+    end
+
+    def move
+      if !@moved
+        if @below == '+'
+          self.intersect
+        end
+        # move cart
+        case @dir
+        when Direction::Up    then @y -= 1
+        when Direction::Right then @x += 1
+        when Direction::Down  then @y += 1
+        when Direction::Left  then @x -= 1
+        end
+      end
+      @moved = true
+    end
+
+    def turn
+      case @below
+      when '/' # turn on curve
+        case @dir
+        when Direction::Up    then @dir = Direction::Right
+        when Direction::Right then @dir = Direction::Up
+        when Direction::Down  then @dir = Direction::Left
+        when Direction::Left  then @dir = Direction::Down
+        end
+      when '\\' # turn on curve
+        case @dir
+        when Direction::Up    then @dir = Direction::Left
+        when Direction::Right then @dir = Direction::Down
+        when Direction::Down  then @dir = Direction::Right
+        when Direction::Left  then @dir = Direction::Up
+        end
+      end
+    end
+
+    def intersect
+      # turn on intersect
+      case @step % 3
+      when 0 # turn left
+        case @dir
+        when Direction::Up    then @dir = Direction::Left
+        when Direction::Right then @dir = Direction::Up
+        when Direction::Down  then @dir = Direction::Right
+        when Direction::Left  then @dir = Direction::Down
+        end
+      when 1 # go straight
+      when 2 # turn right
+        case @dir
+        when Direction::Up    then @dir = Direction::Right
+        when Direction::Right then @dir = Direction::Down
+        when Direction::Down  then @dir = Direction::Left
+        when Direction::Left  then @dir = Direction::Up
+        end
+      end
+      @step += 1
     end
   end
 
   def solution
-    carts = [] of Cart
-    coords = Array.new(@input.size) { |_| Array.new(@input[0].size) { |_| ' ' } }
-
     # Parse input into map and carts
-    Range.new(0, @input.size, true).each do |y|
-      Range.new(0, @input[0].size, true).each do |x|
-        spot = @input[y][x]
-        case spot
+
+    Range.new(0, @coords.size, true).each do |y|
+      Range.new(0, @coords[y].size, true).each do |x|
+        case @coords[y][x]
         when '<'
-          carts << Cart.new(x, y, '-', Direction::Left)
+          @carts << Cart.new(x, y, '-', Direction::Left)
+          @coords[y][x] = '-'
         when '>'
-          carts << Cart.new(x, y, '-', Direction::Right)
+          @carts << Cart.new(x, y, '-', Direction::Right)
+          @coords[y][x] = '-'
         when '^'
-          carts << Cart.new(x, y, '|', Direction::Up)
+          @carts << Cart.new(x, y, '|', Direction::Up)
+          @coords[y][x] = '|'
         when 'v'
-          carts << Cart.new(x, y, '|', Direction::Down)
+          @carts << Cart.new(x, y, '|', Direction::Down)
+          @coords[y][x] = '|'
         end
-        coords[y][x] = @input[y][x]
       end
     end
 
     intersection = {"x" => 0, "y" => 0, "crash" => false, "first" => true}
 
-    while carts.dup.reject! { |oc| oc.crashed }.size > 1
+    while @carts.size > 1
       # Order top-left to bottom-right
-      carts.each { |cart| cart.moved = false }
-      rows = carts.map { |cart| cart.y }.sort.to_set
+      @carts.each { |cart| cart.moved = false }
+      rows = @carts.map { |cart| cart.y }.sort.to_set
       rows.each do |row|
-        carts.dup.reject! { |cart| cart.y != row }.each do |cart|
+        @carts.dup.reject! { |cart| cart.y != row }.each { |cart|
           if !cart.moved && !cart.crashed
-            if cart.below == '+'
-              # turn on intersect
-              case cart.step % 3
-              when 0 # turn left
-                case cart.dir
-                when Direction::Up    then cart.dir = Direction::Left
-                when Direction::Right then cart.dir = Direction::Up
-                when Direction::Down  then cart.dir = Direction::Right
-                when Direction::Left  then cart.dir = Direction::Down
+            # move
+            cart.move
+            cart.below = @coords[cart.y][cart.x]
+            cart.turn
+            @carts.each do |cart2|
+              if cart != cart2
+                if cart.y == cart2.y && cart.x == cart2.x
+                  cart.crashed = true
+                  cart2.crashed = true
+                  # Part 1
+                  if intersection["first"]
+                    intersection["x"] = cart.x
+                    intersection["y"] = cart.y
+                    intersection["crash"] = true
+                    intersection["first"] = false
+                  end
+                  @carts.reject! { |oc| oc.crashed }
                 end
-              when 1 # go straight
-              when 2 # turn right
-                case cart.dir
-                when Direction::Up    then cart.dir = Direction::Right
-                when Direction::Right then cart.dir = Direction::Down
-                when Direction::Down  then cart.dir = Direction::Left
-                when Direction::Left  then cart.dir = Direction::Up
-                end
               end
-              cart.step += 1
             end
-            # replace coords with previous
-            coords[cart.y][cart.x] = cart.below
-            # move cart
-            case cart.dir
-            when Direction::Up    then cart.y -= 1
-            when Direction::Right then cart.x += 1
-            when Direction::Down  then cart.y += 1
-            when Direction::Left  then cart.x -= 1
-            end
-            # swap out chars
-            cart.below = coords[cart.y][cart.x]
-            case cart.dir
-            when Direction::Up    then coords[cart.y][cart.x] = '^'
-            when Direction::Right then coords[cart.y][cart.x] = '>'
-            when Direction::Down  then coords[cart.y][cart.x] = 'v'
-            when Direction::Left  then coords[cart.y][cart.x] = '<'
-            end
-            case cart.below
-            when '/' # turn on curve
-              case cart.dir
-              when Direction::Up    then cart.dir = Direction::Right
-              when Direction::Right then cart.dir = Direction::Up
-              when Direction::Down  then cart.dir = Direction::Left
-              when Direction::Left  then cart.dir = Direction::Down
-              end
-            when '\\' # turn on curve
-              case cart.dir
-              when Direction::Up    then cart.dir = Direction::Left
-              when Direction::Right then cart.dir = Direction::Down
-              when Direction::Down  then cart.dir = Direction::Right
-              when Direction::Left  then cart.dir = Direction::Up
-              end
-            when '<', '>', '^', 'v' # handle crash
-              # Part 1 solution
-              if intersection["first"]
-                intersection["x"] = cart.x
-                intersection["y"] = cart.y
-                intersection["crash"] = true
-                intersection["first"] = false
-              end
-
-              # mark crash
-              carts.dup.reject! { |oc| oc.x != cart.x && oc.y != cart.y }.each { |crashcart| crashcart.crashed = true }
-              cart.crashed = true
-
-              # swap chart
-              coords[cart.y][cart.x] = @input[cart.y][cart.x]
-
-              # remove crashes
-              carts.reject! { |oc| oc.crashed }
-            end
-
-            cart.moved = true
           end
-        end
+        }
       end
     end
 
     # Part 2
-    lastcart = carts.dup.reject! { |oc| oc.crashed }.not_nil![0]
-    result = {"First intersection" => [intersection["x"], intersection["y"]], "Last remaining" => [lastcart.x, lastcart.y]}
-    return result
-  end
-
-  def part2
-    return 0
+    return {"First intersection" => [intersection["x"], intersection["y"]], "Last remaining" => [@carts[0].x, @carts[0].y]}
   end
 end
